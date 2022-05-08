448f22dfb023650cecc9d4e00240312abb8a1223
pkorobov <korobov.i.am@gmail.com>
2022-05-08 23:27:36 +0300

    Make training results reproducable

--- run_experiment.py
+++ run_experiment.py
@@ -1,5 +1,9 @@
 import torch
 from torch.optim import RMSprop
+from argparse import ArgumentParser
+import numpy as np
+import random
+
 
 from wordle_rl.model import RNNAgent
 from wordle_rl.wrappers import nature_dqn_env
@@ -11,16 +15,24 @@ from tqdm import trange
 from wordle_rl.tokenizer import Tokenizer
 
 
-# torch.autograd.set_detect_anomaly(True)
-torch.backends.cudnn.benchmark = True
+def fix_seed(seed):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    random.seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
 
-if __name__ == "__main__":
-
+def main(base_seed, total_steps):
     nenvs = 6
     nsteps = 32
 
-    env = nature_dqn_env(nenvs=nenvs)
+    assert base_seed >= nenvs
+
+    fix_seed(base_seed)
+    env = nature_dqn_env(nenvs=nenvs, seed=[i + base_seed for i in range(nenvs)])
     game_voc_matrix = torch.FloatTensor(env.game_voc_matrix)
 
     obs = env.reset()
@@ -45,11 +57,19 @@ if __name__ == "__main__":
     optimizer = RMSprop(policy.parameters(), 7e-4)
     a2c = A2C(policy, optimizer,  entropy_coef=0.1, max_grad_norm=50.0)
 
-    total_steps = 3 * 10 ** 7
-
     env.reset()
     for step in trange(0, total_steps + 1, nenvs * nsteps):
         if step % 1000 == 0 and step > 0:
             torch.save(a2c.policy.state_dict(), f"model_weights/step_{step}")
 
         a2c.train(runner)
+    return a2c
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('--seed', type=int)
+    parser.add_argument('--total_steps', 10 ** 7, type=int)
+    args = parser.parse_args()
+
+    main(base_seed=args.seed, total_steps=args.total_steps)
--- tests/test_env.py
+++ tests/test_env.py
@@ -1,9 +1,20 @@
 from wordle_rl.wordle_env import WordleEnv
+from wordle_rl.model import RNNAgent
 from wordle_rl.wrappers import nature_dqn_env
+from wordle_rl.runners import EnvRunner
+
+from wordle_rl.transforms import ComputeValueTargets, MergeTimeBatch
+from wordle_rl.a2c import A2C
+from tqdm import trange
+from wordle_rl.tokenizer import Tokenizer
+
 import numpy as np
+import torch
+from torch.optim import RMSprop
+from run_experiment import main
 
 
-def test_deterministic():
+def test_one_deterministic():
     """
     This test checks if seed fixation really works.
     """
@@ -13,7 +24,8 @@ def test_deterministic():
         wordle_env.reset(42)
         words = [wordle_env.word]
         for _ in range(steps):
-            wordle_env.step(np.array([5, 5, 5, 5, 5]))
+            action = np.array([5, 5, 5, 5, 5])
+            wordle_env.step(action)
             if not np.all(wordle_env.word == words[-1]):
                 words.append(wordle_env.word)
         return np.array(words)
@@ -22,3 +34,14 @@ def test_deterministic():
     words2 = get_trajectory_words(100)
 
     assert np.all(words1 == words2), f"{words1} != {words2}"
+
+
+def test_parallel_deterministic():
+    a2c1 = main(100, 1000)
+    a2c2 = main(100, 1000)
+
+    parameters1 = dict(a2c1.policy.named_parameters())
+    parameters2 = dict(a2c2.policy.named_parameters())
+
+    for name, value in parameters1.items():
+        assert torch.allclose(value, parameters2[name])
